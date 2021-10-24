124fbecde424ef652b5f8ebe2c735eb8a3a22b0c
Ancelotow <owen.ancelot.sio@gmail.com>
2021-10-24 11:54:18 +0200

    Start playing

--- sources/src/game.c
+++ sources/src/game.c
@@ -7,6 +7,12 @@
 #define CMD_CREATE_GAME "create"
 #define CMD_LOAD_GAME "load [save_file]"
 
+#define CMD_SAVE 'l'
+#define CMD_UP 'z'
+#define CMD_DOWN 's'
+#define CMD_LEFT 'q'
+#define CMD_RIGHT 'd'
+
 void runGame(){
     char input[50];
     printf("\nSouhaitez-vous creer (%s) ou charger (%s) une partie ?", CMD_CREATE_GAME, CMD_LOAD_GAME);
@@ -22,9 +28,40 @@ void runGame(){
     }
 }
 
+void printAction(){
+    printf("====================================  ACTION  =====================================\n");
+    printf("||                                      ||                                       ||\n");
+    printf("||  [Z] -> deplacement haut             ||   [B] -> retour au menu principal     ||\n");
+    printf("||  [Q] -> deplacement gauche           ||   [L] -> sauvegarde la partie         ||\n");
+    printf("||  [S] -> deplacement bas              ||   [P] -> sauvegarder et quitter       ||\n");
+    printf("||  [D] -> deplacement droite           ||   [C] -> quitter sans sauvegarder     ||\n");
+    printf("||  [I] -> regader mon inventaire       ||                                       ||\n");
+    printf("||                                      ||                                       ||\n");
+    printf("===================================================================================\n");
+}
+
+
+
+void play(World* world){
+    int continueGame = 1;
+    char input;
+    do{
+        printf("\n");
+        printZone(world->originalWorld[0]);
+        printAction();
+        printf("\nQuelle action souhaitez-vous faire ?");
+        scanf("%c", &input); // TODO: Pourquoi il faut 2 scanf ?
+        scanf("%c", &input); // TODO: Pourquoi il faut 2 scanf ?
+        if(input == CMD_SAVE){
+            continueGame = 0;
+        }
+    } while(continueGame);
+    freeWorld(world);
+}
+
 void createVoidPartie(){
     printf("Creation de la partie...\n");
     World* world = generateWorld();
     printf("Partie cree avec succes !\n\n");
-    printZone(world->originalWorld[0]);
+    play(world);
 }
