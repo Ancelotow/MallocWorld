49b6a7db558e9357276c31052df5c53e30c66034
Elsa <elsafirmin@hotmail.fr>
2021-11-14 15:56:35 +0100

    Construction SDL v2

--- cmake-build-debug/CMakeFiles/MallocWorld.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/MallocWorld.dir/DependInfo.cmake
@@ -4,31 +4,17 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/main.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/main.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/action/fight.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/action/fight.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/action/mining.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/action/mining.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/game.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/game.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/global.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/global.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/inventory/inventory.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/inventory/inventory.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/inventory/stack.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/inventory/stack.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/inventory/storage.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/inventory/storage.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/monster.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/monster.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/player.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/player.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/position.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/position.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/world/map/forest.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/world/map/forest.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/world/map/village.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/world/map/village.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/world/respawn.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/world/respawn.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/world/world.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/world/world.c.obj"
-  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/sources/src/world/zone.c" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/sources/src/world/zone.c.obj"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/main.cpp" "C:/Users/Elsa Firmin/CLionProjects/MallocWorld/cmake-build-debug/CMakeFiles/MallocWorld.dir/main.cpp.obj"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sdl/include/SDL2"
   )
 
 # The set of dependency files which are needed:
--- cmake-build-debug/CMakeFiles/MallocWorld.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/MallocWorld.dir/cmake_clean.cmake
@@ -1,20 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/MallocWorld.dir/sources/main.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/action/fight.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/action/mining.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/game.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/global.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/inventory/inventory.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/inventory/stack.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/inventory/storage.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/monster.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/player.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/position.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/world/map/forest.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/world/map/village.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/world/respawn.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/world/world.c.obj"
-  "CMakeFiles/MallocWorld.dir/sources/src/world/zone.c.obj"
+  "CMakeFiles/MallocWorld.dir/main.cpp.obj"
   "MallocWorld.exe"
   "MallocWorld.exe.manifest"
   "MallocWorld.pdb"
@@ -22,6 +7,6 @@ file(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/MallocWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
